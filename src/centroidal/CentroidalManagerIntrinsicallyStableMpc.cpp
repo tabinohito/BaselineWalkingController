275dbeaf89a05f3a602942a54a6fe1fdf1c9bfda
tabinohito <tako.taro.tf@tut.jp>
2024-07-03 16:53:06 +0900

    Remove logging statement in CentroidalManagerIntrinsicallyStableMpc

--- src/centroidal/CentroidalManagerIntrinsicallyStableMpc.cpp
+++ src/centroidal/CentroidalManagerIntrinsicallyStableMpc.cpp
@@ -101,8 +101,6 @@ CCC::IntrinsicallyStableMpc::RefData CentroidalManagerIntrinsicallyStableMpc::ca
       maxPos = maxPos.cwiseMax(pos.head<2>());
     }
   }
-  mc_rtc::log::info("[CentroidalManagerIntrinsicallyStableMpc] minPos: {} maxPos: {}", minPos.transpose(),
-                    maxPos.transpose());
   refData.zmp_limits[0] = minPos;
   refData.zmp_limits[1] = maxPos;
   return refData;
