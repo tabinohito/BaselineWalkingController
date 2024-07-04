dc1fb80b9db895fc8a23851e82ac7e4f6ae0de0e
tabinohito <tako.taro.tf@tut.jp>
2024-07-04 21:37:17 +0900

    Fix contact area calculation and touch down detection in TactileState

--- src/states/TactileState.cpp
+++ src/states/TactileState.cpp
@@ -73,19 +73,33 @@ void TactileState::teardown(mc_control::fsm::Controller &) {}
 
 void TactileState::tactileCallback(const std_msgs::Float32MultiArray::ConstPtr & tactileMsg)
 {
+
   for(auto foot : feet)
   {
     int footIndex_offset = foot == Foot::Left ? 0 : 9;
     auto tactileData = tactileMsg->data;
-    contactArea_[foot].push_back(Eigen::Vector2d(tactileData[0 + footIndex_offset], tactileData[1 + footIndex_offset]));
-    contactArea_[foot].push_back(Eigen::Vector2d(tactileData[2 + footIndex_offset], tactileData[3 + footIndex_offset]));
-    contactArea_[foot].push_back(Eigen::Vector2d(tactileData[4 + footIndex_offset], tactileData[5 + footIndex_offset]));
-    contactArea_[foot].push_back(Eigen::Vector2d(tactileData[6 + footIndex_offset], tactileData[7 + footIndex_offset]));
 
-    min_pose_[foot] = Eigen::Vector3d(tactileData[2 + footIndex_offset], tactileData[7 + footIndex_offset], -0.1065);
-    max_pose_[foot] = Eigen::Vector3d(tactileData[0 + footIndex_offset], tactileData[5 + footIndex_offset], -0.1065);
+    bool touchDown = false;
+    for(const auto & contact : ctl().footManager_->getCurrentContactFeet())
+    {
+      if(contact == foot && !tactileData[8 + footIndex_offset])
+      {
+        touchDown = true;
+      }
+    }
+    
+    if(!touchDown)
+    {
+      contactArea_[foot].push_back(Eigen::Vector2d(tactileData[0 + footIndex_offset], tactileData[1 + footIndex_offset]));
+      contactArea_[foot].push_back(Eigen::Vector2d(tactileData[2 + footIndex_offset], tactileData[3 + footIndex_offset]));
+      contactArea_[foot].push_back(Eigen::Vector2d(tactileData[4 + footIndex_offset], tactileData[5 + footIndex_offset]));
+      contactArea_[foot].push_back(Eigen::Vector2d(tactileData[6 + footIndex_offset], tactileData[7 + footIndex_offset]));
 
+      min_pose_[foot] = Eigen::Vector3d(tactileData[2 + footIndex_offset], tactileData[7 + footIndex_offset], -0.1065);
+      max_pose_[foot] = Eigen::Vector3d(tactileData[0 + footIndex_offset], tactileData[5 + footIndex_offset], -0.1065);
+    }
     touchDown_[foot] = tactileData[8 + footIndex_offset] ? true : false;
+
   }
 }
 
